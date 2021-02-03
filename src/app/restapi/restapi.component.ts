04d015d61cb32fdfe4f01bdbcfd7b6ad8ff31b72
Nicolás Tobo <na.tobo@uniandes.edu.co>
2021-02-03 05:27:59 -0500

    Funciona Rest Call

--- src/app/restapi/restapi.component.ts
+++ src/app/restapi/restapi.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthorizationService} from "../shared/authorization.service";
-import {Http, Headers} from "@angular/http";
+import { AuthorizationService } from "../shared/authorization.service";
+import { Http, Headers } from "@angular/http";
 
+const apiUrl = "https://xewt0rniyc.execute-api.us-east-1.amazonaws.com/Prod/Lambda_api-lambda-db-tiendaback-nicotobo";
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 export class PersonWithCars {
-  constructor(public name: string, public age: number) { }  
+  constructor(public name: string, public age: number) { }
 }
 
 @Component({
@@ -13,7 +15,8 @@ export class PersonWithCars {
 })
 export class RestApiComponent implements OnInit {
 
-  _data : any;
+  _data: any;
+  Items: any[];
 
   constructor(private http: Http, private auth: AuthorizationService) { }
 
@@ -22,32 +25,37 @@ export class RestApiComponent implements OnInit {
     if (authenticatedUser == null) {
       return;
     }
-    authenticatedUser.getSession( (err, session) => {
+    authenticatedUser.getSession((err, session) => {
       if (err) {
         console.log(err);
         return;
       }
-      const token = session.getIdToken().getJwtToken();      
+      const token = session.getIdToken().getJwtToken();
       const headers = new Headers();
-      headers.append('Authorization', token);      
+      headers.append('Authorization', token);
       var that = this;
       this.auth.getAuthenticatedUser().getSession((err, session) => {
         if (err) {
           console.log(err);
           return;
         }
-        const token = session.getIdToken().getJwtToken();        
+        const token = session.getIdToken().getJwtToken();
         const headers = new Headers();
-        headers.append('Authorization', token);        
-        this.http.get('put your api gateway endpoint here', { headers: headers })
+        headers.append('Authorization', token);
+        this.http.post(proxyurl + apiUrl, {
+          "operation": "list",
+          "tableName": "Dynamo_api-lambda-db-tiendaback-nicotobo",
+          "payload": {}
+        }, { headers: headers })
           .subscribe(
-          response => {           
-            that._data = response.json();
-          },
-          error => {
-            console.log(error);
-          }
-        );
+            response => {
+              that._data = response.json();
+              that.Items = that._data.Items;
+            },
+            error => {
+              console.log(error);
+            }
+          );
       });
     });
   }
